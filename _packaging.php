0f1afd58649987f085210ff999213ba997811177
Eccenux <eccenux+git@gmail.com>
2012-05-11 12:34:17 +0000

    packaging JS with PHP

---
+++ _packaging.php
@@ -0,0 +1,38 @@
+<?php
+	header("Content-Type: text/plain");
+	
+	@require_once "./_packaging.class.php";
+	$strThisDir = rtrim(dirname(__FILE__), "/\ ");
+	$oLoader = new ecSimpleJSLoader($strThisDir);
+	
+	// package app
+	$oLoader->noCache = true;
+	$oLoader->isLeaveMultiCommentsWithCopyright = true;
+	$oLoader->strBaseModulesName = 'mJqmApplication.';
+	$oLoader->strMiniModulesName = 'mJqmApp.mini.js';
+	$strMiniFileName = $oLoader->createMiniModules(array(
+		 'base'
+		,'utils'
+		,'utils.geo'
+		,'i18n'
+		,'storage'
+		,'model'
+		,'controller'
+		,'setup'
+		,'forms'
+	));
+	
+	// package libs
+	$oLoader->noCache = true;
+	$oLoader->isLeaveMultiCommentsWithCopyright = false;
+	$oLoader->isRemoveInlineComments = false;
+	$oLoader->strBaseModulesName = 'lib\\';
+	$oLoader->strMiniModulesName = 'lib\\libs.comb.js';
+	$strMiniFileName = $oLoader->createMiniModules(array(
+		 'jquery.store'
+		,'jquery.metadata'
+		,'jquery.validate'
+		,'form_cr'
+		,'I18n'
+	));
+?>
